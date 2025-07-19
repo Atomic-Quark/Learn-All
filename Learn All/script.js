d786183e00e4b4e396c557e9f8bc5bd7352dc2e1
Divyanshu Kumar <143479650+Atomic-Quark@users.noreply.github.com>
2025-07-19 20:24:26 +0530

    Delete Learn All directory

--- Learn All/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', (e) => {
-        e.preventDefault();
-        window.location.href = card.getAttribute('href');
-    });
-});
